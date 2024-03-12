6832efb32a9c06d12f99cc143bb0922dcd7326d2
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-12 15:10:34 +0000

    useeffect called

---
+++ my-app/src/LearnUseEffect.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+// import { useEffect } from 'react'
+
+const LearnUseEffect=()=> {
+  const [count,setCount]=useState(0)
+  const handleCount =()=>{
+    setCount(count +1)
+  }
+  const [randomNumber,setRandomNumber]=useState(null);
+  const generateRandomNumber =()=>{
+    const newRandomNumber =Math.floor(Math.random()*100)+1;
+    setRandomNumber(newRandomNumber);
+  }
+  useEffect(()=>{
+console.log("UseEffect called");
+  })
+  return (
+    <div>
+      <h1>LearnUseEffect</h1>
+      <h2>Count : {count}</h2>
+      <button onClick={handleCount}>Increase Count</button>
+      <hr/>
+      <h1>LearnUseEffect</h1>
+      <h2>randomNumber : {randomNumber}</h2>
+      <button onClick={generateRandomNumber}>RandomNumber</button>
+    </div>
+  )
+}
+
+export default LearnUseEffect;
